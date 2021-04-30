0f0700c25ee8f5983508fc961b169d8b2d2e9245
ANANYA MAHAJAN <ananya.mahajan22@gmail.com>
2021-04-30 10:06:09 +0530

    Highest Altitude

---
+++ src/Highest_Altitude.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Highest_Altitude {
+    public int largestAltitude(int[] gain) {
+        int altitude[]=new int[gain.length+1];
+        altitude[0]=0;
+        for(int i=1;i<altitude.length;i++)
+        {
+            altitude[i]=altitude[i-1]+gain[i-1];
+        }
+        Arrays.sort(altitude);
+        return altitude[altitude.length-1];
+    }
+}
