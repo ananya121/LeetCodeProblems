9d1769ec26724ddd9356ffab5b2c04ba0d57b688
ANANYA MAHAJAN <ananya.mahajan22@gmail.com>
2021-05-01 20:19:01 +0530

    Number of Students Doing Homework at a Given Time

---
+++ src/NumberOfStudentsDoingHomeworkAtGivenTime.java
@@ -0,0 +1,12 @@
+
+public class NumberOfStudentsDoingHomeworkAtGivenTime {
+	public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count=0;
+        for(int i=0;i<startTime.length;i++)
+        {
+            if(queryTime>=startTime[i] && queryTime<=endTime[i])   
+                count++;
+        }
+        return count;
+    }
+}
