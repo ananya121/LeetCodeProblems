58940ed77b4cdbf4d8f451ae9a44d5562ef2cdd8
ANANYA MAHAJAN <ananya.mahajan22@gmail.com>
2021-05-02 19:40:33 +0530

    Final Prices With a Special Discount in a Shop

---
+++ src/FinalPricesWithSpecialDiscountInShop.java
@@ -0,0 +1,19 @@
+
+public class FinalPricesWithSpecialDiscountInShop {
+	public int[] finalPrices(int[] prices) {
+        int n =prices.length;
+        for(int i=0;i<n;i++)
+        {
+            int index=-1;
+            for(int j=i+1;j<n;j++)
+            {
+                if(prices[j]<=prices[i])
+                {
+                    prices[i]=prices[i]-prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+}
