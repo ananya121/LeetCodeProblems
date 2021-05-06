fa6c36d50e205bc84030f1a57e1b5bac746e0afe
ANANYA MAHAJAN <ananya.mahajan22@gmail.com>
2021-05-06 19:45:58 +0530

    Arrays Partition 1

---
+++ src/ArrayPartition_1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ArrayPartition_1 {
+	public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length;i=i+2)
+        {
+            sum=sum+nums[i];
+        }
+        return sum;
+    }
+}
